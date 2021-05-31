e7193617e12d037f1800cfc532914a8a5334e6b1
dnjsqlsgml <dnjsqlsgml@naver.com>
2021-05-31 06:17:36 +0000

    Update

---
+++ bo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define MY_SHA3_HASH_LAST_TWO_DIGIT_NUMBER 92
+
+int func1(){
+	printf("Fail..\n");
+	return -1;
+}
+
+int func2(){
+	printf("My password is Success!!!\n");
+	return 1;
+}
+
+void main1(){
+	char buffer[MY_SHA3_HASH_LAST_TWO_DIGIT_NUMBER];
+	scanf("%s", buffer);
+	func1();
+}
+
+void main(){
+	main1();
+}
